df01098073ac425844aea9ddc022a60e1ceb9de4
msinotebook <tngurdl00@hanmail.net>
2017-05-14 17:37:28 +0900

    Update SingleMeshGenerater

---
+++ Assets/Scripts/SingleMeshGenerater.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class ModifyMeshEvent : UnityEngine.Events.UnityEvent<Mesh> { }
+
+/// <summary>
+/// pick mesh in meshfilter and pass mesh to event.
+/// </summary>
+public class SingleMeshGenerater : MonoBehaviour
+{
+    MeshRenderer meshRenderer;
+    MeshFilter filter;
+
+    [SerializeField]
+    public ModifyMeshEvent modifyEvent;
+
+    void Generate()
+    {
+        filter = GetComponent<MeshFilter>();
+
+        if (filter == null)
+            filter = gameObject.AddComponent<MeshFilter>();
+
+        meshRenderer = GetComponent<MeshRenderer>();
+
+        if (meshRenderer == null)
+            meshRenderer = gameObject.AddComponent<MeshRenderer>();
+
+        Mesh mesh = filter.sharedMesh == null ? new Mesh() : filter.sharedMesh;
+
+        if (modifyEvent != null)
+            modifyEvent.Invoke(mesh);
+
+        filter.sharedMesh = mesh;
+    }
+
+    void Awake()
+    {
+        Generate();
+    }
+}
