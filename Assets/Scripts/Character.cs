9a22d7c6f2b0c6bd1f043bfd1eda3ca27739a352
zizon <tngurdl00@hanmail.net>
2017-05-18 15:02:14 +0900

    Update minecraft character modeling, rigging, skinning

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SkinnedMeshRenderer))]
+public class Character : MonoBehaviour
+{
+    [SerializeField]
+    private Material material;
+
+    SkinnedMeshRenderer skinnedMeshRenderer;
+
+    void Awake()
+    {
+        skinnedMeshRenderer = GetComponent<SkinnedMeshRenderer>();
+
+        if (skinnedMeshRenderer == null)
+            skinnedMeshRenderer = gameObject.AddComponent<SkinnedMeshRenderer>();
+
+        Mesh mesh = new Mesh();
+
+        Vector3[] vertices = null;
+        int[] indices = null;
+
+        VertexMapper.GetVertices(ref vertices);
+        VertexMapper.GetIndices(ref indices);
+
+        mesh.vertices = vertices;
+        mesh.triangles = indices;
+
+        Vector2[] uvs = null;
+        UVMapper.GetUV(ref uvs);
+        mesh.uv = uvs;
+
+        Transform[] boneArray = Rigger.GetBoneTransform(gameObject);
+
+        Matrix4x4[] bindPoses = null;
+        BoneWeight[] weight = null;
+
+        SkinMapper.GetBoneWieghts(ref weight);
+        Rigger.GetBindPoses(ref bindPoses, transform, boneArray);
+
+        mesh.boneWeights = weight;
+        mesh.bindposes = bindPoses;
+
+        skinnedMeshRenderer.sharedMesh = mesh;
+        skinnedMeshRenderer.material = material;
+        skinnedMeshRenderer.bones = boneArray;
+        skinnedMeshRenderer.rootBone = transform;
+    }
+}
---
+++ Assets/Scripts/Rigger.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Rigger
+{
+    public static float defaultUnitSize = 0.125f;
+    public static Vector3[] defaultBodyPosArray =
+        new Vector3[] { new Vector3(0, 12f, 0), new Vector3(0, 12f, 0), new Vector3(-3f, 12f - 1f, 0), new Vector3(3f, 12f - 1f, 0), new Vector3(-1, 6f, 0), new Vector3(1f, 6f, 0), };
+
+    public static Transform[] GetBoneTransform(GameObject rootObject, Vector3[] bodyPosArray = null)
+    {
+        bodyPosArray = defaultBodyPosArray;
+        Transform[] bodyArray = new Transform[bodyPosArray.Length];
+
+        for (int i = 0; i < bodyPosArray.Length; i++)
+        {
+            GameObject bone = new GameObject(string.Format("Bone{0}", i));
+
+            bone.transform.parent = rootObject.transform;
+            bone.transform.localPosition = bodyPosArray[i] * defaultUnitSize;
+
+            bodyArray[i] = bone.transform;
+        }
+
+        return bodyArray;
+    }
+
+    public static void GetBindPoses(ref Matrix4x4[] bindPoses, Transform root, Transform[] bones)
+    {
+        if (bindPoses == null)
+            bindPoses = new Matrix4x4[bones.Length];
+        else if (bindPoses.Length < bones.Length)
+            Array.Resize(ref bindPoses, bones.Length);
+
+        for (int i = 0; i < bones.Length; i++)
+            bindPoses[i] = bones[i].worldToLocalMatrix * root.localToWorldMatrix;
+    }
+
+}
