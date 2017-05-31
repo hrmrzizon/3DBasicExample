95576311ff9193d06d3361c2d66b899885dba2a9
zizon <tngurdl00@hanmail.net>
2017-05-31 16:53:40 +0900

    Update animting for normal

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -7,7 +7,7 @@ using UnityEngine;
 public class Character : MonoBehaviour
 {
     [SerializeField]
-    private Shader shader;
+    private Material material;
 
     [SerializeField]
     private CharacterData data;
@@ -30,7 +30,6 @@ public class Character : MonoBehaviour
         if (animator == null)
             animator = gameObject.AddComponent<Animator>();
 
-        Material material = new Material(shader);
         material.SetTexture("_MainTex", data.bodyTex);
 
         Mesh mesh = new Mesh();
